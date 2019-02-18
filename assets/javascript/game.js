b2f93f58a76e15cd228d9d5c2acce140a649474f
pgonzalez003 <pgonzalez003@gmail.com>
2019-02-17 16:41:38 -0800

    here

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -28,41 +28,39 @@ $(".btn-primary").on("click", function(){
     	console.log("davidButton pressed");
     	counter += characters.davidButton;
     	console.log(counter);
-        
+        alert(characters.davidButton)
     }
     else if (this.id == "xenomorphButton"){
     	console.log("xenomorphButton pressed");
     	counter += characters.xenomorphButton;
     	console.log(counter);
-        
+        alert(characters.xenomorphButton)
     }
     else if (this.id == "predatorsButton"){
     	console.log("predatorsButton pressed");
     	counter += characters.predatorsButton;
         console.log(counter);
-        
+        alert(characters.predatorsButton)
     }
     else
     {
     	console.log("engineerButton pressed");
     	counter += characters.engineerButton;
         console.log(counter);
-        
+        alert(characters.engineerButton)
     }
-
-    alert(counter)
 
     if (counter == targetNumber){
         alert("LIFT OFF!!!");
     }
     else if (counter <= targetNumber){
-        alert(counter + "  fuel cells collected!");
+        alert(counter + "  Fuel Cells Collected!");
     }
     else { 
         counter >= targetNumber 
-        alert("Food for the facehuggers");
+        alert("Food for the Facehuggers");
         counter = 0, startGame();
     }
-    
+    $("#fuelMeter").text(counter);
 
 })
